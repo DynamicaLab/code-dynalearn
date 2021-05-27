e1b3b366188923ec33e004dd57634324c1ffe641
Charles Murphy <charles.murphy.1@ulaval.ca>
2021-05-27 16:33:04 -0400

    removing misc in tests

--- tests/experiments/test_experiment.py
+++ tests/experiments/test_experiment.py
@@ -1,5 +1,7 @@
 import numpy as np
+import os
 import unittest
+
 from dynalearn.config import ExperimentConfig
 from dynalearn.experiments import Experiment
 
@@ -12,9 +14,11 @@ class ExperimentTest(unittest.TestCase):
 
     def tearDown(self):
         self.experiment.end()
+        if os.path.exists("test.pt"):
+            os.remove("test.pt")
 
     def test_generate(self):
-        self.experiment.generate_data()
+        self.experiment.generate_data(save=False)
         expected = (
             self.config.train_details.num_samples
             * self.config.train_details.num_networks
@@ -23,13 +27,13 @@ class ExperimentTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_train(self):
-        self.experiment.generate_data()
+        self.experiment.generate_data(save=False)
         self.experiment.partition_val_dataset()
-        self.experiment.train_model()
+        self.experiment.train_model(save=False)
         logs = self.experiment.model.nn.history._epoch_logs
         for epoch, log in logs.items():
             self.assertFalse(np.isnan(log["loss"]))
 
     def test_compute_metrics(self):
-        self.experiment.generate_data()
-        self.experiment.compute_metrics()
+        self.experiment.generate_data(save=False)
+        self.experiment.compute_metrics(save=False)
--- tests/test_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for d in */ ; do
-    echo "$d"
-    cd $d
-    python -m unittest discover -v
-    cd ../
-done
